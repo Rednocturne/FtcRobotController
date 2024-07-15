9ac3e8f0e74d4f7eebea213fe26e7466413308c2
Roan <sansediscord@gmail.com>
2024-07-14 19:22:13 -0700

    Made the code uniform with if statements. Added more comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -22,6 +22,7 @@ public class MecanumTeleOp extends LinearOpMode {
 
 
     //Defining variables
+    //reaperPos is the position that the reaper will be in.
     int reaperPos = 1;
 
     public void runOpMode() {
@@ -72,8 +73,12 @@ public class MecanumTeleOp extends LinearOpMode {
             backRightMotor.setPower(backRightPower);
 
             //This is setting the intake power when a bumper is pressed.
-            if (gamepad1.right_bumper) intake.setPower(.8);
-            else if (gamepad1.left_bumper) intake.setPower(-0.8);
+            if (gamepad1.right_bumper) {
+                intake.setPower(.8);
+            }
+            else if (gamepad1.left_bumper) {
+                intake.setPower(-0.8);
+            }
             else intake.setPower(0);
 
             //This means that, whenever the intake is active, the hood will be down.
@@ -83,6 +88,7 @@ public class MecanumTeleOp extends LinearOpMode {
             else Hood.setPosition(.4325);
 
             //This is a rising edge detector. It cycles between 3 states when a is pressed, adding one to reaperPos after each press. When reaperPos is above 3, it gets set back to 1.
+            //This code is changing what position the reaper is at based on what value reaperPos has.
             if (currentGamepad1.a && !previousGamepad1.a) {
                 reaperPos++;
             }
