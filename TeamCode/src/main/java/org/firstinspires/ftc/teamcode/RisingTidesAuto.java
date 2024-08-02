0e70edfbf42b9b668b7a0a1f45d5d747030f9e06
Roan <sansediscord@gmail.com>
2024-08-01 18:38:43 -0700

    Update RisingTidesAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RisingTidesAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RisingTidesAuto.java
@@ -28,177 +28,194 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
 @Autonomous(name = "RisingTidesAuto", group = "Autonomous")
-public  class RisingTidesAuto extends LinearOpMode {}
+public class RisingTidesAuto extends LinearOpMode {
 
-// intake class
-public static class intake {
-    private DcMotorEx intake;
+    // intake class
+    public static class intake {
+        private DcMotorEx intake;
 
-    public intake(HardwareMap hardwareMap) {
-        intake = hardwareMap.get(DcMotorEx.class, "intake");
-        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        public intake(HardwareMap hardwareMap) {
+            intake = hardwareMap.get(DcMotorEx.class, "intake");
+            intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        public class intakeOn implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPower(0.8);
+                return false;
+            }
+        }
+
+        public Action intakeOn() {
+            return new intakeOn();
+        }
+
+        public class intakeOff implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPower(0);
+                return false;
+            }
+        }
+
+        public Action intakeOff() {
+            return new intakeOff();
+        }
+
+
     }
 
-    public class intakeOn implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            intake.setPower(0.8);
-            return false;
+    // reaper class
+    public static class reaper {
+        private static Servo reaper;
+
+        static final double stow = 0.7;
+        static final double stack = 0.18;
+        static final double ground = 0.11;
+
+        public reaper(HardwareMap hardwareMap) {
+            Servo reaper = hardwareMap.get(Servo.class, "reaper");
+        }
+
+        public static class reaperStow implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                reaper.setPosition(stow);
+                return false;
+            }
+        }
+
+        public static Action reaperStow() {
+            return new reaperStow();
+        }
+
+        public static class reaperStack implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                reaper.setPosition(stack);
+                return false;
+            }
+        }
+
+        public static Action reaperStack() {
+            return new reaperStack();
+        }
+
+        public static class reaperGround implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                reaper.setPosition(ground);
+                return false;
+            }
+        }
+
+        public static Action reaperGround() {
+            return new reaperGround();
         }
     }
-    public Action intakeOn() {
-        return new intakeOn();
-    }
 
-    public class intakeOff implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            intake.setPower(0);
-            return false;
+    public static class hood {
+        private Servo hood;
+
+        final double hoodOpen = 0.57;
+        final double hoodClose = 0.4425;
+
+        public hood(HardwareMap hardwareMap) {
+            hood = hardwareMap.get(Servo.class, "hood");
         }
-    }
-    public Action intakeOff() {
-        return new intakeOff();
-    }
 
-
-}
-
-// reaper class
-public static class reaper {
-    private static Servo reaper;
-
-    static final double stow = 0.7;
-    final double stack = 0.18;
-    static final double ground = 0.11;
-
-    public reaper(HardwareMap hardwareMap) {
-        Servo reaper = hardwareMap.get(Servo.class, "reaper");
-    }
-    public static class reaperStow implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            reaper.setPosition(stow);
-            return false;
+        public class hoodOpen implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                hood.setPosition(hoodOpen);
+                return false;
+            }
         }
-    }
-    public static Action reaperStow() {
-        return new reaperStow();
-    }
-    public static class reaperStack implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            reaper.setPosition(stack);
-            return false;
+
+        public Action hoodOpen() {
+            return new hoodOpen();
         }
-    }
-    public static Action reaperStack() {
-        return new reaperStack();
-    }
-    public static class reaperGround implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            reaper.setPosition(ground);
-            return false;
+
+        public class hoodClose implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                hood.setPosition(hoodClose);
+                return false;
+            }
         }
-    }
-    public static Action reaperGround() {
-        return new reaperGround();
-    }
-}
-public static class hood {
-    private Servo hood;
 
-    final double hoodOpen = 0.57;
-    final double hoodClose =  0.4425;
-
-    public hood(HardwareMap hardwareMap) {
-        hood = hardwareMap.get(Servo.class, "hood");
-    }
-    public class hoodOpen implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            hood.setPosition(hoodOpen);
-            return false;
+        public Action hoodClose() {
+            return new hoodClose();
         }
-    }
-    public Action hoodOpen() {
-        return new hoodOpen();
+
     }
 
-    public class hoodClose implements Action {
-        @Override
-        public boolean run(@NonNull TelemetryPacket packet) {
-            hood.setPosition(hoodClose);
-            return false;
+    @Override
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(90)));
+        // make a Claw instance
+        intake intake = new intake(hardwareMap);
+        // make a Lift instance
+        hood hood = new hood(hardwareMap);
+
+        // vision here that outputs position
+        int visionOutputPosition = 1;
+
+        Action trajectoryAction1;
+        Action trajectoryAction2;
+        Action trajectoryAction3;
+        Action trajectoryActionCloseOut;
+
+        trajectoryAction1 = drive.actionBuilder(drive.pose)
+                .setTangent(0)
+                .splineTo(new Vector2d(50, 0), Math.PI / 2)
+                .waitSeconds(2)
+                .lineToX(30)
+                .build();
+        trajectoryAction2 = drive.actionBuilder(drive.pose)
+                .build();
+        trajectoryAction3 = drive.actionBuilder(drive.pose)
+                .build();
+        trajectoryActionCloseOut = drive.actionBuilder(drive.pose)
+                .build();
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            int position = visionOutputPosition;
+            telemetry.addData("Position during Init", position);
+            telemetry.update();
         }
-    }
-    public Action hoodClose() {
-        return new hoodClose();
-    }
-
-}
-@Override
-public void runOpMode() {
-    // instantiate your MecanumDrive at a particular pose.
-    MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(90)));
-    // make a Claw instance
-    intake intake = new intake(hardwareMap);
-    // make a Lift instance
-    hood hood = new hood(hardwareMap);
-
-    // vision here that outputs position
-    int visionOutputPosition = 1;
-
-    Action trajectoryAction1;
-    Action trajectoryAction2;
-    Action trajectoryAction3;
-
-    trajectoryAction1 = drive.actionBuilder(drive.pose)
-    .setTangent(0)
-            .splineTo(new Vector2d(50, 0), Math.PI / 2)
-            .waitSeconds(2)
-            .lineToX(30)
-            .build();
-    Actions.runBlocking(hood.hoodClose());
-    Actions.runBlocking(reaper.reaperStow());
-
-    while (!isStopRequested() && !opModeIsActive()) {
-        int position = visionOutputPosition;
-        telemetry.addData("Position during Init", position);
+        int startPosition = visionOutputPosition;
+        telemetry.addData("Starting Position", startPosition);
         telemetry.update();
+        waitForStart();
+
+
+        if (isStopRequested()) return;
+
+
+        Action trajectoryActionChosen;
+        if (startPosition == 1) {
+            trajectoryActionChosen = trajectoryAction1;
+        } else if (startPosition == 2) {
+            trajectoryActionChosen = trajectoryAction2;
+        } else {
+            trajectoryActionChosen = trajectoryAction3;
+        }
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen,
+                        reaper.reaperStack(),
+                        intake.intakeOn(),
+                        hood.hoodOpen(),
+                        reaper.reaperStow(),
+                        intake.intakeOff(),
+                        hood.hoodClose(),
+                        trajectoryActionCloseOut
+                )
+        );
     }
-    int startPosition = visionOutputPosition;
-    telemetry.addData("Starting Position", startPosition);
-    telemetry.update();
-    waitForStart();
-
-
-    if (isStopRequested()) return;
-
-
-    Action trajectoryActionChosen;
-    if (startPosition == 1) {
-        trajectoryActionChosen = trajectoryAction1;
-    } else if (startPosition == 2) {
-        trajectoryActionChosen = trajectoryAction2;
-    } else {
-        trajectoryActionChosen = trajectoryAction3;
-    }
-
-    Actions.runBlocking(
-            new SequentialAction(
-                    trajectoryActionChosen,
-                    reaper.reaperStack(),
-                    intake.intakeOn(),
-                    hood.hoodOpen(),
-                    reaper.reaperStow(),
-                    intake.intakeOff(),
-                    hood.hoodClose(),
-                    trajectoryActionCloseOut
-            )
-    );
-
-
 }
